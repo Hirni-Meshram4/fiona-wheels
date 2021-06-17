3f4ee9de23aa1c2d7d0a3c959ab95dccaa7c2286
Hirni-Meshram4 <85619553+Hirni-Meshram4@users.noreply.github.com>
2021-06-17 15:33:30 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -93,7 +93,6 @@ function build_curl {
         LIBS=-ldl ./configure $flags; else \
         ./configure $flags; fi\
 	&& echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" \
-       # && make -j4 \
 	&& echo "***********************************************" \
         && make install)
     touch curl-stamp
